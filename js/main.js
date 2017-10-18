14f98ede295dc1959edab76d936450a0aae1ed16
Ross Z Bernet <rbernet@azavea.com>
2017-10-17 21:25:42 -0400

    adding 3d and text

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $('.nav-tabs a[href="#1"]').click(function(){
   document.getElementById('map_container_county').style.visibility = 'hidden';
   document.getElementById('map_container_compare_a').style.visibility = 'hidden';
   document.getElementById('map_container_compare_b').style.visibility = 'hidden';
-  document.getElementById('map_container_nyc3d').style.visibility = 'hidden';
+  document.getElementById('map_container_3d').style.visibility = 'hidden';
 });
 
 $('.nav-tabs a[href="#2"]').click(function(){
@@ -13,7 +13,7 @@ $('.nav-tabs a[href="#2"]').click(function(){
   document.getElementById('map_container_county').style.visibility = 'visible';
   document.getElementById('map_container_compare_a').style.visibility = 'hidden';
   document.getElementById('map_container_compare_b').style.visibility = 'hidden';
-  document.getElementById('map_container_nyc3d').style.visibility = 'hidden';
+  document.getElementById('map_container_3d').style.visibility = 'hidden';
 });
 
 $('.nav-tabs a[href="#3"]').click(function(){
@@ -21,7 +21,7 @@ $('.nav-tabs a[href="#3"]').click(function(){
   document.getElementById('map_container_county').style.visibility = 'hidden';
   document.getElementById('map_container_compare_a').style.visibility = 'visible';
   document.getElementById('map_container_compare_b').style.visibility = 'visible';
-  document.getElementById('map_container_nyc3d').style.visibility = 'hidden';
+  document.getElementById('map_container_3d').style.visibility = 'hidden';
 });
 
 $('.nav-tabs a[href="#4"]').click(function(){
@@ -29,12 +29,12 @@ $('.nav-tabs a[href="#4"]').click(function(){
   document.getElementById('map_container_county').style.visibility = 'hidden';
   document.getElementById('map_container_compare_a').style.visibility = 'hidden';
   document.getElementById('map_container_compare_b').style.visibility = 'hidden';
-  document.getElementById('map_container_nyc3d').style.visibility = 'visible';
+  document.getElementById('map_container_3d').style.visibility = 'visible';
 });
 
 // CITY DATA POINTS ADD TO MAP
 
-map_city.on('style.load', function() { //style load - retain nfip layer with style change
+map_city.on('style.load', function() { //style load
   map_city.addSource('city_points', {
           type: 'geojson',
           data: 'https://raw.githubusercontent.com/rossbernet/penn_final/master/us_cities_1_copy_1.geojson'
@@ -137,8 +137,141 @@ map_county.on('zoom', function() {
 });
 
 
+// Comparing maps - MAP A
+
+map_compare_a.on('style.load', function() { //style load
+  map_compare_a.addSource('city_points', {
+          type: 'geojson',
+          data: 'https://raw.githubusercontent.com/rossbernet/penn_final/master/us_cities_1_copy_1.geojson'
+  });
+      map_compare_a.addLayer({
+          'id': 'points',
+          'type': 'circle',
+          'source': 'city_points',
+          'layout': {
+              'visibility': 'visible',
+          },
+          'paint': {
+        'circle-radius':1.5,
+        'circle-color': {
+          stops: [
+            [0, '#2DC4B2']
+          ]
+        },
+        'circle-opacity': 0.9
+      }
+    });
+});
 
 
+// Comparing maps - MAP B
+
+map_compare_b.on('style.load', function() { //style load - retain nfip layer with style change
+  map_compare_b.addSource('population', {
+          'type': 'vector',
+          'url': 'mapbox://mapbox.660ui7x6'
+      });
+      var zoomThreshold = 4;
+      map_compare_b.addLayer({
+          'id': 'state-population',
+          'source': 'population',
+          'source-layer': 'state_county_population_2014_cen',
+          'maxzoom': zoomThreshold,
+          'type': 'fill',
+          'filter': ['==', 'isState', true],
+          'paint': {
+              'fill-color': {
+                  property: 'population',
+                  stops: [
+                      [0, '#F2F12D'],
+                      [500000, '#EED322'],
+                      [750000, '#E6B71E'],
+                      [1000000, '#DA9C20'],
+                      [2500000, '#CA8323'],
+                      [5000000, '#B86B25'],
+                      [7500000, '#A25626'],
+                      [10000000, '#8B4225'],
+                      [25000000, '#723122']
+                  ]
+              },
+              'fill-opacity': 0.75
+          }
+      }, 'waterway-label');
+
+
+      map_compare_b.addLayer({
+        'id': 'county-population',
+        'source': 'population',
+        'source-layer': 'state_county_population_2014_cen',
+        'minzoom': zoomThreshold,
+        'type': 'fill',
+        'filter': ['==', 'isCounty', true],
+        'paint': {
+            'fill-color': {
+                property: 'population',
+                stops: [
+                    [0, '#F2F12D'],
+                    [100, '#EED322'],
+                    [1000, '#E6B71E'],
+                    [5000, '#DA9C20'],
+                    [10000, '#CA8323'],
+                    [50000, '#B86B25'],
+                    [100000, '#A25626'],
+                    [500000, '#8B4225'],
+                    [1000000, '#723122']
+                ]
+            },
+            'fill-opacity': 0.75
+        }
+    }, 'waterway-label');
+
+
+});
+
+
+var stateLegendEl = document.getElementById('state-legend');
+var countyLegendEl = document.getElementById('county-legend');
+map_compare_b.on('zoom', function() {
+  var zoomThreshold = 4;
+    if (map_compare_b.getZoom() > zoomThreshold) {
+        stateLegendEl.style.display = 'none';
+        countyLegendEl.style.display = 'block';
+    } else {
+        stateLegendEl.style.display = 'block';
+        countyLegendEl.style.display = 'none';
+    }
+});
+
+
+
+map_3d.on('load', function() {
+    // Insert the layer beneath any symbol layer.
+    var layers = map_3d.getStyle().layers.reverse();
+    var labelLayerIdx = layers.findIndex(function (layer) {
+        return layer.type !== 'symbol';
+    });
+    var labelLayerId = labelLayerIdx !== -1 ? layers[labelLayerIdx].id : undefined;
+    map_3d.addLayer({
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 15,
+        'paint': {
+            'fill-extrusion-color': '#aaa',
+            'fill-extrusion-height': {
+                'type': 'identity',
+                'property': 'height'
+            },
+            'fill-extrusion-base': {
+                'type': 'identity',
+                'property': 'min_height'
+            },
+            'fill-extrusion-opacity': .6
+        }
+    }, labelLayerId);
+});
 
 
 
--- js/setup.js
+++ js/setup.js
@@ -3,43 +3,59 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoicm9zc2Jlcm5ldCIsImEiOiJRLTh3WEVNIn0.7S-AEC6js
 var map_city = new mapboxgl.Map({
   container: 'map_container_city', // container element id
   style: 'mapbox://styles/mapbox/dark-v9',
-  center: [-95.7129, 37.0902], // initial map center in [lon, lat]
+  center: [-95, 37], // initial map center in [lon, lat]
   zoom: 3
 });
 
+map_city.addControl(new mapboxgl.NavigationControl());
 
 var map_county = new mapboxgl.Map({
   container: 'map_container_county', // container element id
-  style: 'mapbox://styles/mapbox/dark-v9',
-  center: [-95.7129, 37.0902], // initial map center in [lon, lat]
-  zoom: 4
-});
-
-
-var map_nyc3d = new mapboxgl.Map({
-  container: 'map_container_nyc3d', // container element id
-  style: 'mapbox://styles/mapbox/dark-v9',
-  center: [-95.7129, 37.0902], // initial map center in [lon, lat]
-  zoom: 6
+  style: 'mapbox://styles/mapbox/light-v9',
+  center: [-95, 37], // initial map center in [lon, lat]
+  zoom: 3
 });
 
 
 // COMPARE MAPS
 var map_compare_a = new mapboxgl.Map({
     container: 'map_container_compare_a',
-    style: 'mapbox://styles/mapbox/light-v9',
-    center: [0, 0],
-    zoom: 0
+    style: 'mapbox://styles/mapbox/dark-v9',
+    center: [-95, 37], // initial map center in [lon, lat]
+    zoom: 3
 });
 
 var map_compare_b = new mapboxgl.Map({
     container: 'map_container_compare_b',
-    style: 'mapbox://styles/mapbox/dark-v9',
-    center: [0, 0],
-    zoom: 0
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: [-95, 37], // initial map center in [lon, lat]
+    zoom: 3
 });
 
 var map = new mapboxgl.Compare(map_compare_a, map_compare_b, {
     // Set this to enable comparing two maps by mouse movement:
     // mousemove: true
 });
+
+
+// Map in 3d
+
+
+var map_3d = new mapboxgl.Map({
+  // container: 'map_container_nyc3d', // container element id
+  // style: 'mapbox://styles/mapbox/dark-v9',
+  // center: [-75.16, 34.95],
+  // zoom: 13,
+  // pitch: 45,
+  // bearing: -17.6,
+  // hash: true
+    style: 'mapbox://styles/mapbox/dark-v9',
+    center: [-75.167, 39.9524],
+    zoom: 15,
+    pitch: 136,
+    bearing: 60,
+    hash: true,
+    container: 'map_container_3d'
+});
+
+map_3d.addControl(new mapboxgl.NavigationControl());
